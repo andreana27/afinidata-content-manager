ad5b32641a0e442d6d73f5afea947a16feddf9bc
Sebastian Oliva <code@sebastianoliva.com>
2019-10-15 04:06:18 -0600

    silly py35 crap

--- messenger_users/models.py
+++ messenger_users/models.py
@@ -89,7 +89,8 @@ class UserActivityLog(models.Model):
     success = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.user}: {self.initial_state} -> {self.final_state} [{self.timestamp}]"
+        # return f"{self.user}: {self.initial_state} -> {self.final_state} [{self.timestamp}]"
+        return "{}: {} -> {} -- {}".format(self.user, self.initial_state, self.final_state, self.timestamp)
 
 
 class UserActivity(models.Model):
