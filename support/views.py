16369128bede422029f698c30303b4ce38ec8378
Boris Aguilar <baguilar@innovare.es>
2020-03-30 12:41:14 +0400

    Adding new support module, for initial testing

--- support/views.py
+++ support/views.py
@@ -22,7 +22,7 @@ def chatfuel(request):
     bot_id = '5e4cdf014b1fd70001e9384b'
     user_id = '3650968864944027'
     token = 'mELtlMAHYqR0BvgEiMq8zVek3uYUK3OJMbtyrdNPTrQB9ndV0fM7lWTFZbM4MZvD'
-    block_name = 'SEQUENCE_TEST'
+    block_name = 'subscribe_sequence'
     url = f'''https://api.chatfuel.com/bots/{bot_id}/users/{user_id}/send?chatfuel_token={token}&chatfuel_message_tag=Update&chatfuel_block_name={block_name}&{url_part}'''
     r = requests.post(url)
     return HttpResponse(r.text)
