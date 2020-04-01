faa5726406309a0d0103133b377b48558dd0085a
Boris Aguilar <baguilar@innovare.es>
2020-04-01 14:15:53 +0400

    Adding Support views

--- support/views.py
+++ support/views.py
@@ -15,16 +15,24 @@ def index(request):
 
 @csrf_exempt
 def chatfuel(request):
+    #https://contentmanager.afinidata.com/utilities/child_months/?date=14/06/1989
+    vars = ['child_dob', 'childDOBinput', 'childDOB', 'favorite_birthday']
     url_part = ''
-    attributes = [dict(attribute_name = '',
-                       attribute_value = '')]
+    date_of_birth = request.POST.get("dob"),
+    attributes = []
+    for v in vars:
+        attributes += dict(v, date_of_birth)
+    r = requests.get(request.build_absolute_uri(reverse('utilities:get_months'),
+                     params = dict(date = date_of_birth))
+    cm = json.loads(r.text)['set_attributes']['childMonths']
+    attributes += dict(childMonths = cm)
     for attribute_name, attribute_value in attributes:
         url_part += f'{attribute_name}={attribute_value}&'
     bot_id = '5e4cdf014b1fd70001e9384b'
-    user_id = '3650968864944027'
+    user_id = request.POST.get("fb_user_id")#'3650968864944027'
     token = 'mELtlMAHYqR0BvgEiMq8zVek3uYUK3OJMbtyrdNPTrQB9ndV0fM7lWTFZbM4MZvD'
-    block_name = 'subscribe_sequence'
+    block_name = request.POST.get("block_name")
     #tag = ''&chatfuel_message_tag={tag}
     url = f'''https://api.chatfuel.com/bots/{bot_id}/users/{user_id}/send?chatfuel_token={token}&chatfuel_block_name={block_name}&{url_part}'''
     r = requests.post(url)
-    return HttpResponse(r.text)
+    return return render(request, 'support/result.html', dict(message = json.loads(r.text)))
