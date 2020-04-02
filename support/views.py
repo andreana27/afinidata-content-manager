a1760dd454ef81707a8a2d1f73dd34b3add19f2d
Boris Aguilar <baguilar@innovare.es>
2020-04-02 17:51:12 +0400

    Adding support selection for bots

--- support/views.py
+++ support/views.py
@@ -15,6 +15,8 @@ def index(request):
 
 @csrf_exempt
 def chatfuel(request):
+    def get_fb_user_id(user_id):
+        return User.objects.get(id=user_id).last_channel_id
     #https://contentmanager.afinidata.com/utilities/child_months/?date=14/06/1989
     vars = ['child_dob', 'childDOBinput', 'childDOB', 'favorite_birthday']
     url_part = ''
@@ -28,11 +30,16 @@ def chatfuel(request):
     attributes['childMonths'] = cm
     for attribute_name, attribute_value in attributes.items():
         url_part += f'{attribute_name}={attribute_value}&'
+    #Afini Tutor
     bot_id = '5e4cdf014b1fd70001e9384b'
-    user_id = request.POST.get("fb_user_id")#'3650968864944027'
     token = 'mELtlMAHYqR0BvgEiMq8zVek3uYUK3OJMbtyrdNPTrQB9ndV0fM7lWTFZbM4MZvD'
+    #Afini normal
+    if request.POST.get("bot") == "1":
+        bot_id = '58bcd41ee4b0d01fda181a35'
+        token = 'vnbqX6cpvXUXFcOKr5RHJ7psSpHDRzO1hXBY8dkvn50ZkZyWML3YdtoCnKH7FSjC'
+    fb_user_id = get_fb_user_id(request.POST.get("user_id"))#'3650968864944027'
     block_name = request.POST.get("block_name")
     #tag = ''&chatfuel_message_tag={tag}
-    url = f'''https://api.chatfuel.com/bots/{bot_id}/users/{user_id}/send?chatfuel_token={token}&chatfuel_block_name={block_name}&{url_part}'''
+    url = f'''https://api.chatfuel.com/bots/{bot_id}/users/{fb_user_id}/send?chatfuel_token={token}&chatfuel_block_name={block_name}&{url_part}'''
     r = requests.post(url)
     return render(request, 'support/result.html', dict(message = json.loads(r.text)))
