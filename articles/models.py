6a6ccb4ad8212bcf62abbfc3272ff5bea89fe965
Estuardo Diaz <estuardoremi@gmail.com>
2020-08-18 02:07:37 -0600

    hotfix

--- articles/models.py
+++ articles/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Topic(models.Model):
-    id = models.CharField(max_length=35, primary_key=True)
-    name = models.CharField(max_length=140)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
 
 class Topic(models.Model):
     id = models.CharField(max_length=35, primary_key=True)
