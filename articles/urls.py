36e020aa2fa5baf7eb7cc8f1f9847e7cd9fa90d5
Estuardo Diaz <estuardoremi@gmail.com>
2020-08-18 00:17:02 -0600

    gitflow-feature-stash: article_topics

--- articles/urls.py
+++ articles/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', views.ArticleListView.as_view(), name='articles_list'),
     path('create/', views.ArticleCreateView.as_view(), name='article_create'),
     path('<int:article_id>/', views.ArticleDetailView.as_view(), name='article_detail'),
-    path('<int:article_id>/edit/', views.ArticleUpdateView.as_view(), name='article_edit')
+    path('<int:article_id>/edit/', views.ArticleUpdateView.as_view(), name='article_edit'),
+    path('', views.TopicListView.as_view(), name='topics_list'),
+    path('topic/create/', views.TopicCreateView.as_view(), name='topic_create'),
+    path('topic/<int:topic_id>/', views.TopicDetailView.as_view(), name='topic_detail'),
+    path('topic/<int:topic_id>/edit/', views.TopicUpdateView.as_view(), name='topic_edit')
 ]
