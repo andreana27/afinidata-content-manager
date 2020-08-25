553bee902132191377284f4bc1ac85714bc75079
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-08-25 17:28:46 -0600

    Added model for translation

--- languages/models.py
+++ languages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from milestones.models import Milestone
 
 CODE_ORIGIN = (('generated', 'Generated'), ('created', 'Created'))
 
@@ -25,3 +26,13 @@ class LanguageCode(models.Model):
 
     def __str__(self):
         return self.code
+
+
+class MilestoneTranslation(models.Model):
+    milestone = models.ForeignKey(Milestone, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    language_code = models.ForeignKey(LanguageCode, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
