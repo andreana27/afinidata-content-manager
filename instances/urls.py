fce1fd43c3ef9a89318aef1748662eb46fa3af93
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-10-11 19:13:09 -0600

    Changed list

--- instances/urls.py
+++ instances/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
          name='complete_milestone'),
     path('<int:instance_id>/reverse_milestone/<int:milestone_id>/', views.ReverseMilestoneView.as_view(),
          name='reverse_milestone'),
+    path('<int:instance_id>/dont_know_milestone/<int:milestone_id>/', views.DontKnowMilestoneView.as_view(),
+         name='dont_know_milestone'),
     path('<int:instance_id>/add_attribute/', views.AddAttributeToInstanceView.as_view(), name='add_instance_attribute'),
     path('<int:instance_id>/edit_attribute/<int:attribute_id>/', views.AttributeValueEditView.as_view(),
          name='edit_instance_attribute'),
