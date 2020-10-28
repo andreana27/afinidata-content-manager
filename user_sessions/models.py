3ddc36f1b420c871801cdbd2982c7a29e28b7b6e
Estuardo Diaz <estuardoremi@gmail.com>
2020-10-27 23:26:15 -0600

    gitflow-hotfix-stash: session_interactions_foreignkey_name

--- user_sessions/models.py
+++ user_sessions/models.py
@@ -89,8 +89,8 @@ class Interaction(models.Model):
     """
     session = models.ForeignKey(Session, on_delete=models.CASCADE, null=True)
     field = models.ForeignKey(Field, on_delete=models.CASCADE, null=True)
-    user_id = models.ForeignKey(MessengerUser, on_delete=models.CASCADE, null=True)
-    instance_id = models.ForeignKey(Instance, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(MessengerUser, on_delete=models.CASCADE, null=True)
+    instance = models.ForeignKey(Instance, on_delete=models.CASCADE, null=True)
     bot_id = models.IntegerField(default=1)
     type = models.CharField(max_length=255, default='open')
     value = models.IntegerField(default=0, null=True)
