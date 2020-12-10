f34afef9345d075f24a048b9c63ade7a7b54ee4e
Jose Quintero <jg.fernandez31@gmail.com>
2020-12-10 19:54:52 -0400

    add axios library for ajax request

--- posts/static/js/post.js
+++ posts/static/js/post.js
@@ -1,45 +1,46 @@
 (() => {
     window.addEventListener('load', () => {
-
         const POST = document.querySelector('#post-article')
         const DOMAIN =  window.location.origin
-
-        console.log(DOMAIN)
-
+        // console.log(DOMAIN)
         if (POST) {
             const SESSION_ID = POST.dataset.sessionId
-            console.log(POST)
+            // console.log(POST)
 
             if (SESSION_ID !== 'null') {
                 let minutes = 0
-
                 setTimeout(() => {
                     let form = new FormData()
                     form.append('minutes', 0)
                     const URI = `${DOMAIN}/posts/interaction/${SESSION_ID}/edit/`
-                    fetch(URI, {
-                        method: 'post',
-                        body: form
-                    })
-                        .then(response => response.json())
-                        .then(response => console.log(response))
+                    const updateInteracion = async () => {
+                      try{
+                        resp = await axios.post(URI, form)
+                        console.log(resp);
+                      }catch(err){
+                        console.log(err);
+                      }
+                    }
+                    updateInteracion();
                 }, 5000)
+
                 let requestInterval = setInterval(() => {
                     let form = new FormData()
-                    console.log(minutes)
                     if(minutes >= 20) {
                         clearInterval(requestInterval)
                     }
                     minutes = minutes + 1
                     form.append('minutes', minutes)
                     const URI = `${DOMAIN}/posts/interaction/${SESSION_ID}/edit/`
-                    console.log(URI)
-                    fetch(URI, {
-                        method: 'post',
-                        body: form
-                    })
-                        .then(response => response.json())
-                        .then(response => console.log(response))
+                    const updateInteracion = async () => {
+                      try {
+                        resp = await axios.post(URI, form);
+                        console.log(resp);
+                      } catch(err){
+                        console.log(err)
+                      }
+                    }
+                    updateInteracion();
                 }, (60 * 1000))
             }
         }
