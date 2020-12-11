9c4f08660a9588d463fd6f00ab4b46cd6c697897
Jose Quintero <jg.fernandez31@gmail.com>
2020-12-11 08:25:14 -0400

    make ajax request with vanilla js

--- posts/static/js/post.js
+++ posts/static/js/post.js
@@ -1,48 +1,44 @@
-(() => {
-    window.addEventListener('load', () => {
-        const POST = document.querySelector('#post-article')
-        const DOMAIN =  window.location.origin
-        // console.log(DOMAIN)
-        if (POST) {
-            const SESSION_ID = POST.dataset.sessionId
-            // console.log(POST)
+window.addEventListener('load', () => {
+  const POST = document.querySelector('#post-article')
+  const DOMAIN =  window.location.origin
 
-            if (SESSION_ID !== 'null') {
-                let minutes = 0
-                setTimeout(() => {
-                    let form = new FormData()
-                    form.append('minutes', 0)
-                    const URI = `${DOMAIN}/posts/interaction/${SESSION_ID}/edit/`
-                    const updateInteracion = async () => {
-                      try{
-                        resp = await axios.post(URI, form)
-                        console.log(resp);
-                      }catch(err){
-                        console.log(err);
-                      }
-                    }
-                    updateInteracion();
-                }, 5000)
-
-                let requestInterval = setInterval(() => {
-                    let form = new FormData()
-                    if(minutes >= 20) {
-                        clearInterval(requestInterval)
-                    }
-                    minutes = minutes + 1
-                    form.append('minutes', minutes)
-                    const URI = `${DOMAIN}/posts/interaction/${SESSION_ID}/edit/`
-                    const updateInteracion = async () => {
-                      try {
-                        resp = await axios.post(URI, form);
-                        console.log(resp);
-                      } catch(err){
-                        console.log(err)
-                      }
-                    }
-                    updateInteracion();
-                }, (60 * 1000))
-            }
+  function updateInteraction(url, payload){
+    xhr = new XMLHttpRequest();
+    xhr.open('POST',url, true);
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState == 4){
+        if(xhr.status == 200){
+          console.log(xhr.responseText);
+        } else {
+          console.log("error");
         }
-    })
-})()
+      }
+    }
+    xhr.send(payload);
+  }
+
+  if (POST) {
+      const SESSION_ID = POST.dataset.sessionId
+
+      if (SESSION_ID !== 'null') {
+          let minutes = 0
+          setTimeout(() => {
+              let form = new FormData()
+              form.append('minutes', 0)
+              const URI = `${DOMAIN}/posts/interaction/${SESSION_ID}/edit/`
+              updateInteraction(URI, form);
+          }, 5000)
+
+          let requestInterval = setInterval(() => {
+              let form = new FormData()
+              if(minutes >= 20) {
+                  clearInterval(requestInterval)
+              }
+              minutes = minutes + 1
+              form.append('minutes', minutes)
+              const URI = `${DOMAIN}/posts/interaction/${SESSION_ID}/edit/`
+              updateInteraction(URI, form);
+          }, (60 * 1000))
+      }
+  }
+})
