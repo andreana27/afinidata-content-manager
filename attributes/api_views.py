75d411206d56301bc3f71ed06407673d267c9ce8
Estuardo Diaz <estuardoremi@gmail.com>
2021-02-02 16:52:46 -0600

    Add id filter

--- attributes/api_views.py
+++ attributes/api_views.py
@@ -7,5 +7,11 @@ class AttributeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Attribute.objects.all()
     serializer_class = serializers.AttributeSerializer
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.query_params.get('id'):
+            return qs.filter(id=self.request.query_params.get('id'))
+        return qs
+
     def list(self, request, *args, **kwargs):
         return super(AttributeViewSet, self).list(request, *args, **kwargs)
