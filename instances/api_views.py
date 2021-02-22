317840d8ab2f863ca37d054b9190b37cdd5a262d
Jose Quintero <jg.fernandez31@gmail.com>
2021-02-22 14:36:42 -0400

    fix error en api para busqueda de attributes

--- instances/api_views.py
+++ instances/api_views.py
@@ -1,8 +1,8 @@
-from rest_framework import viewsets, permissions
-from instances import models, serializers
-from django.utils.decorators import method_decorator
 from django.db.models import Q
-
+from rest_framework import filters
+from instances import models, serializers
+from rest_framework import viewsets, permissions
+from django.utils.decorators import method_decorator
 
 class InstanceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Instance.objects.all()
@@ -24,10 +24,11 @@ class InstanceViewSet(viewsets.ReadOnlyModelViewSet):
 
         return qs
 
-
 class InstancesAttributeViewSet(viewsets.ModelViewSet):
     queryset = models.AttributeValue.objects.all()
     serializer_class = serializers.AttributeValueSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ("$attribute__name","$value")
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -39,16 +40,5 @@ class InstancesAttributeViewSet(viewsets.ModelViewSet):
 
         if self.request.query_params.get('instance_id'):
             return qs.filter(instance_id=self.request.query_params.get('instance_id'))
-
-        if self.request.query_params.get('attribute_value') or self.request.query_params.get('attribute_name'):
-
-            a_name = self.request.query_params.get('attribute_name')
-            a_value = self.request.query_params.get('attribute_value')
-
-            filter_by = Q( attribute__name__contains = a_name ) if a_name else False
-            if a_value:
-                filter_by = filter_by & Q( value__contains = a_value ) if filter_by else Q( value__contains = a_value )
-            
-            return qs.filter(filter_by)
 
         return qs
--- messenger_users/api_views.py
+++ messenger_users/api_views.py
@@ -1,11 +1,9 @@
+import logging
+from django.db.models import Q
+from rest_framework import filters
 from rest_framework import viewsets, permissions
 from messenger_users import models, serializers
 from django.utils.decorators import method_decorator
-from django.db.models import Q
-
-
-import logging
-
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.User.objects.all()
@@ -18,10 +16,11 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
         return qs
 
-
 class UserDataViewSet(viewsets.ModelViewSet):
     queryset = models.UserData.objects.all()
     serializer_class = serializers.UserDataSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ("$data_key", "$data_value")
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -33,16 +32,5 @@ class UserDataViewSet(viewsets.ModelViewSet):
 
         if self.request.query_params.get('attribute_id'):
             return qs.filter(attribute_id=self.request.query_params.get('attribute_id'))
-        
-        if self.request.query_params.get('attribute_value') or self.request.query_params.get('attribute_name'):
-
-            a_name = self.request.query_params.get('attribute_name')
-            a_value = self.request.query_params.get('attribute_value')
-
-            filter_by = Q( attribute__name__contains = a_name ) if a_name else False
-            if a_value:
-                filter_by = filter_by & Q( data_value__contains = a_value ) if filter_by else Q( data_value__contains = a_value )
-            
-            return qs.filter(filter_by)
 
         return qs
