e59b73a6e22a77fe72a5659c523e91ab9bc75cc4
Gabriela Herrera <sgherrerapoggio@gmail.com>
2021-03-03 14:51:44 -0600

    Program_Posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from posts.models import PostComplexity
+from posts.models import Area, Materiales, PostComplexity, Post, Situacional
 
 
 class PostComplexitySerializer(serializers.ModelSerializer):
@@ -7,3 +7,38 @@ class PostComplexitySerializer(serializers.ModelSerializer):
     class Meta:
         model = PostComplexity
         fields = ('post', 'user_id', 'months', 'complexity')
+
+
+class AreaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Area
+        fields = ('id', 'name')
+
+
+class MaterialesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Materiales
+        fields = ('id', 'name')
+
+
+class SituacionalSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Situacional
+        fields = ('id', 'name')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    area = AreaSerializer()
+    materiales = MaterialesSerializer(read_only=True, many=True)
+    situacional = SituacionalSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'name', 'status', 'type', 'content', 'content_activity', 'preview', 'new',
+        'thumbnail', 'area', 'materiales', 'situacional', 'preparacion', 'cantidad_materiales', 
+        'ar_id', 'integrantes', 'min_range', 'max_range', 'tiempo_duracion')
+
+
