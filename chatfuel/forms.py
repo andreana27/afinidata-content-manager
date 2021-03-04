d9694463bcd0744afcac847b03a6f41235b32f35
Estuardo Diaz <estuardoremi@gmail.com>
2021-03-04 09:56:03 -0600

    gitflow-hotfix-stash: set_default_date_accept_range

--- chatfuel/forms.py
+++ chatfuel/forms.py
@@ -146,5 +146,4 @@ class SessionFieldReplyForm(forms.Form):
 
 
 class SetDefaultDateValueForm(forms.Form):
-    level_number = forms.ModelChoiceField(queryset=Program.objects.get(id=1).levels.all())
     instance = forms.ModelChoiceField(queryset=Instance.objects.all())
