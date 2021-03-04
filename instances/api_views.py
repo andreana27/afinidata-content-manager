4cd9cc50c839b9ee18289a5ef1b48f56812a1495
Jose Quintero <jg.fernandez31@gmail.com>
2021-03-03 21:05:08 -0400

    people filter advance in instances

--- instances/api_views.py
+++ instances/api_views.py
@@ -1,8 +1,11 @@
 from django.db.models import Q
 from rest_framework import filters
 from instances import models, serializers
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, views
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django.utils.decorators import method_decorator
+from rest_framework.pagination import PageNumberPagination
 
 class InstanceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Instance.objects.all()
@@ -23,6 +26,49 @@ class InstanceViewSet(viewsets.ReadOnlyModelViewSet):
             return qs.filter(id__in=instances)
 
         return qs
+
+    @action(methods=['POST'], detail=False)
+    def advance_search(self, request):
+        queryset = models.Instance.objects.order_by('-id').all()
+        filtros = request.data['filtros']
+        apply_filters = Q()
+        next_connector = None
+
+        for idx, f in enumerate(filtros):
+            #attribute
+            if f['search_by'] == 'attribute':
+                field_name = 'attributevalue__value'
+
+            # TODO: segment
+            # TODO: blocks
+            # TODO: sequence
+
+            if f['condition'] == 'is':
+                query_search = Q(**{f"{field_name}__icontains": f["data_value"]})
+            elif f['condition'] == 'is_not':
+                query_search = ~Q(**{f"{field_name}__icontains": f["data_value"]})
+            elif f['condition'] == 'startswith':
+                query_search = Q(**{f"{field_name}__startswith": f["data_value"]})
+            elif f['condition'] == 'gt':
+                query_search = Q(**{f"{field_name}__gt": f["data_value"]})
+            elif f['condition'] == 'lt':
+                query_search = Q(**{f"{field_name}__lt": f["data_value"]})
+
+            if next_connector is None:
+                apply_filters = Q(attributes__id=f['data_key']) & query_search
+            else:
+                if next_connector == 'and':
+                    apply_filters &= Q(attributes__id=f['data_key']) & query_search
+                else:
+                    apply_filters |= Q(attributes__id=f['data_key']) & query_search
+
+            next_connector = f['connector']
+
+        queryset = queryset.filter(apply_filters)
+        pagination = PageNumberPagination()
+        qs = pagination.paginate_queryset(queryset, request)
+        serializer = serializers.InstanceSerializer(qs, many=True)
+        return pagination.get_paginated_response(serializer.data)
 
 class InstancesAttributeViewSet(viewsets.ModelViewSet):
     queryset = models.AttributeValue.objects.all()
