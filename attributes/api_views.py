165dfbca4acbc09b497462e360732f7d59561b46
haroldolopezpruebas <haroldolopezpruebas@gmail.com>
2021-03-22 00:45:02 -0600

    Agregando cambios al API Entities y Attributes entrevista tecnica

--- attributes/api_views.py
+++ attributes/api_views.py
@@ -3,12 +3,20 @@ from attributes import models, serializers
 from django.utils.decorators import method_decorator
 from entities.models import Entity
 from instances.models import Instance
+from attributes import models as modelAttribute
 from messenger_users.models import User
+from rest_framework.response import Response
+from .paginations import LimitOffsetPagination
+from rest_framework.decorators import action
+from attributes import serializers as attrSerializers
+from .filters import AttrFilter
 
+class AttributeViewSet(viewsets.ModelViewSet):
 
-class AttributeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Attribute.objects.all()
     serializer_class = serializers.AttributeSerializer
+    pagination_class = LimitOffsetPagination
+    filterset_class = AttrFilter
 
     def paginate_queryset(self, queryset, view=None):
 
@@ -18,6 +26,7 @@ class AttributeViewSet(viewsets.ReadOnlyModelViewSet):
         return self.paginator.paginate_queryset(queryset, self.request, view=self)
 
     def get_queryset(self):
+
         qs = super().get_queryset()
 
         if (self.request.query_params.get('id') and self.request.query_params.get('type')
@@ -42,3 +51,24 @@ class AttributeViewSet(viewsets.ReadOnlyModelViewSet):
 
     def list(self, request, *args, **kwargs):
         return super(AttributeViewSet, self).list(request, *args, **kwargs)
+
+    def update(self, request, pk=None):
+
+        name = self.request.data.get("name")
+        type = self.request.data.get("type")
+        attribute_view = self.request.data.get("attribute_view")
+
+        update_attr = self.get_object()
+
+        update_attr.name = name
+        update_attr.type = type
+        update_attr.attribute_view = attribute_view
+
+        update_attr.save()
+
+        serializer_context = {
+            'request': request,
+        }
+
+        serializer = serializers.AttributeSerializer(instance=update_attr,context=serializer_context)
+        return Response(serializer.data)
--- entities/api_views.py
+++ entities/api_views.py
@@ -1,17 +1,67 @@
 from rest_framework import viewsets, permissions
 from entities import models, serializers
+from attributes import models as modelAttribute
 from django.utils.decorators import method_decorator
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.decorators import api_view
+from attributes import serializers as attrSerializers
+from .paginations import LimitOffsetPagination
+from django_filters import rest_framework as filters
+from .filters import EntityFilter
 
+class EntityViewSet(viewsets.ModelViewSet):
 
-class EntityViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Entity.objects.all()
     serializer_class = serializers.EntitySerializer
+    pagination_class = LimitOffsetPagination
+    filterset_class = EntityFilter
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        if self.request.query_params.get('id'):
-            return qs.filter(id=self.request.query_params.get('id'))
-        return qs
+    def create(self, request, *args, **kwargs):
+        data = request.data
+
+        new_entity = models.Entity.objects.create(name=data["name"], description=data["description"])
+        new_entity.save()
+
+        for attribute in data["attributes"]:
+            attribute_obj = modelAttribute.Attribute.objects.get(name=attribute["name"])
+            new_entity.attributes.add(attribute_obj)
+
+        serializer = serializers.EntitySerializer(new_entity)
+
+        return Response(serializer.data)
 
     def list(self, request, *args, **kwargs):
         return super(EntityViewSet, self).list(request, *args, **kwargs)
+    
+    def update(self, request, pk=None):
+
+        name = self.request.data.get("name")
+        description = self.request.data.get("description")
+        attributes = self.request.data.get("attributes")
+
+        update_entity = self.get_object()
+
+        if name is not None:
+            update_entity.name = name
+
+        if description is not None:
+            update_entity.description = description
+
+        update_entity.attributes.set([])
+
+        update_entity.save()
+
+        if attributes is not None:
+            for attribute in attributes:
+                attribute_obj = modelAttribute.Attribute.objects.get(id=attribute["id"]) 
+                update_entity.attributes.add(attribute_obj)
+
+        update_entity.save()
+
+        serializer_context = {
+            'request': request,
+        }
+
+        serializer = serializers.EntitySerializer(instance=update_entity,context=serializer_context)
+        return Response(serializer.data)
