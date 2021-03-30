643fc019d476156aa6d3bcb1ff7e4299d36e3127
Gabriela Herrera <sgherrerapoggio@gmail.com>
2021-03-30 10:22:59 -0600

    Cherry picked from entities

--- attributes/api_views.py
+++ attributes/api_views.py
@@ -1,17 +1,22 @@
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from django.utils.decorators import method_decorator
 
 from attributes import models, serializers
 from entities.models import Entity
 from instances.models import Instance
 from messenger_users.models import User
-
+from .paginations import LimitOffsetPagination
+from .filters import AttrFilter
 
 class AttributeViewSet(viewsets.ModelViewSet):
+
     queryset = models.Attribute.objects.all().order_by('-id')
     serializer_class = serializers.AttributeSerializer
     http_method_names = ['get', 'post', 'options', 'head']
+    pagination_class = LimitOffsetPagination
+    filterset_class = AttrFilter
 
     def paginate_queryset(self, queryset, view=None):
 
@@ -21,6 +26,7 @@ class AttributeViewSet(viewsets.ModelViewSet):
         return self.paginator.paginate_queryset(queryset, self.request, view=self)
 
     def get_queryset(self):
+
         qs = super().get_queryset()
 
         if (self.request.query_params.get('id') and self.request.query_params.get('type')
@@ -51,4 +57,23 @@ class AttributeViewSet(viewsets.ModelViewSet):
         attribute = self.get_serializer(attribute)
         return Response({'status': 201 if created else 200, 'data': attribute.data})
         
-        
+    def update(self, request, pk=None):
+
+        name = self.request.data.get("name")
+        type = self.request.data.get("type")
+        attribute_view = self.request.data.get("attribute_view")
+
+        update_attr = self.get_object()
+
+        update_attr.name = name
+        update_attr.type = type
+        update_attr.attribute_view = attribute_view
+
+        update_attr.save()
+
+        serializer_context = {
+            'request': request,
+        }
+
+        serializer = serializers.AttributeSerializer(instance=update_attr,context=serializer_context)
+        return Response(serializer.data)
