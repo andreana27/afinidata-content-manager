96179615382f085ed60e1d2f36897c25a7e29bdd
Jose Quintero <jg.fernandez31@gmail.com>
2021-04-23 16:11:04 -0400

    update script translate post to pt

--- scripts/translatePosts.py
+++ scripts/translatePosts.py
@@ -8,31 +8,39 @@ import requests
 import csv
 import time
 
+AFINICONTENT_URL = "https://afinicontent.com"
+
 def add(x, y):
     return x + y
 
 def translate_locale_posts(language_origin = 'es',
                            language_destination = 'en',
                            locale_destination = 'en_US'):
+
     def generate_csv(list, filename):
         keys = list[0].keys()
         with open(filename, 'w', newline='') as output_file:
             dict_writer = csv.DictWriter(output_file, keys)
             dict_writer.writeheader()
             dict_writer.writerows(list)
+
     def get_post_from_wordpress(post_slug):
         import requests
-        r = requests.get('https://activities.afinidata.com/wp-json/wp/v2/posts?slug=%s' % (post_slug))
+        r = requests.get('%s/wp-json/wp/v2/posts?slug=%s' % (AFINICONTENT_URL, post_slug))
         #get imitating-animal-sounds
         post = None
         try:
             post = r.json()[0]
         except:
-            raise Exception(dict(request = 'https://activities.afinidata.com/wp-json/wp/v2/posts?slug=%s' % (post_slug)))
+            raise Exception(dict(request = '%s/wp-json/wp/v2/posts?slug=%s' % (AFINICONTENT_URL, post_slug)))
         return post
-    def save_post_wordpress(post_slug, post_title, post_content):
-        url_srcdest = "https://activities.afinidata.com/wp-json/wp/v2/posts/"
-        code = 'bHVjaTp3TWFLIEIxbFMgTFBDNiBqamFxIHl2UmMgSjEzUwo='#str(base64.b64encode(b'luci:wMaK B1lS LPC6 jjaq yvRc J13S'), 'utf-8')
+
+    def save_post_wordpress(post_slug, post_title, post_content, featured_media):
+        url_srcdest = AFINICONTENT_URL + "/wp-json/wp/v2/posts/"
+        # code = 'bHVjaTp3TWFLIEIxbFMgTFBDNiBqamFxIHl2UmMgSjEzUwo='#str(base64.b64encode(b'luci:wMaK B1lS LPC6 jjaq yvRc J13S'), 'utf-8')
+
+        code = str(base64.b64encode(b'luci:NGV8 3x5L kZ0m QENi qZEA XavL'),'utf-8')
+
         headers = {'Content-Type': 'application/json',
                  'Authorization': 'Basic %s' % (code),
                  'Username': 'luci',
@@ -42,38 +50,49 @@ def translate_locale_posts(language_origin = 'es',
                 "title" : post_title,
                 "content" : post_content,
                 "status" : "draft",
-                "slug" : post_slug
+                "slug" : post_slug,
+                "featured_media":featured_media
             }
+
         response = requests.post(url_srcdest, data=json.dumps(data), headers=headers)
+
         if response.status_code < 199 or response.status_code > 300:
             raise Exception(dict(code = response.status_code, response = response.json(), request = data))
-        return 'https://activities.afinidata.com/%s/' % (post_slug)
+
+        return '%s/%s/' % (AFINICONTENT_URL, post_slug)
 
     #Get the URLs of posts that language_origin matches PostLocale,
     #but have no language_destination PostLocale
     done = PostLocale.objects.filter(lang=language_destination)
     excluded_posts = set()
+
     for d in done:
         excluded_posts.add(d.post)
+
     post_locales_to_translate = PostLocale.objects \
       .exclude(post__in=excluded_posts) \
       .filter(lang = language_origin)
+
     # Get al posts that doesnt have an lenguage_destination translation
     done = Post.objects.filter(postlocale__lang=language_destination)
     excluded_posts = set()
+
     for d in done:
         excluded_posts.add(d.id)
+
     post_to_translate = Post.objects.exclude(id__in=excluded_posts) \
-        .filter(content__startswith='https://activities.afinidata.com/') \
-        .filter(id__gt=965)
+        .filter(content__startswith=AFINICONTENT_URL)
 
     translated_posts = []
+
     for post in post_to_translate:
         post_name = post.content \
-          .replace('https://activities.afinidata.com/', '') \
+          .replace(AFINICONTENT_URL+'/', '') \
           .replace('/', '')
+
         # Get the content from the wordpress API
         wordpress_post = get_post_from_wordpress(post_slug = post_name)
+
         # Separate text to avoid translating HTML tags, we only need to translate plain text
         text_html = " " + wordpress_post['content']['rendered'].replace("^", "")
         title = post.name
@@ -81,6 +100,7 @@ def translate_locale_posts(language_origin = 'es',
         tags_array = []
         plain_text_array = []
         i = 0
+
         while i < len(text_html):
             text = ""
             while i < len(text_html) and text_html[i] != "<":
@@ -100,8 +120,10 @@ def translate_locale_posts(language_origin = 'es',
 
         # Translate
         translate = boto3.client(service_name='translate', region_name='us-east-2', use_ssl=True)
+
         # Wait to avoid ThrottlingException
         time.sleep(20)
+
         plain_text = translate \
             .translate_text(Text=plain_text,
                             SourceLanguageCode=language_origin,
@@ -112,6 +134,7 @@ def translate_locale_posts(language_origin = 'es',
         plain_text_array = plain_text.split(delimiter)[1:]
 
         translated_text_html = ""
+
         for i in range(len(plain_text_array)):
             if i < len(plain_text_array):
                 translated_text_html += plain_text_array[i]
@@ -121,7 +144,8 @@ def translate_locale_posts(language_origin = 'es',
         #Create the new translated Wordpress Page, save_new_post_in_wordpress
         url = save_post_wordpress(post_slug = language_destination+'-%s' % (post_name),
                                   post_title = title,
-                                  post_content = translated_text_html)
+                                  post_content = translated_text_html,
+                                  featured_media=wordpress_post['featured_media'])
         #save postLocale
         new_post_locale = PostLocale(lang=language_destination,
                                    locale=locale_destination,
@@ -130,13 +154,17 @@ def translate_locale_posts(language_origin = 'es',
                                    link_post=url,
                                    post_id=post.id)
         new_post_locale.save()
-        print("ID:", post.id, "Translated:", title)
+
+        print("ID:", post.id, "Translated:", title, "Image:")
+
         translated_posts.append(dict(post = post.id,
                                      post_locale=new_post_locale.id,
                                      post_name = post.name,
                                      url = url))
+
     generate_csv(list = translated_posts, filename = 'output.csv')
+
     return dict(translated = len(post_to_translate), posts = translated_posts)
 
 
-translate_locale_posts(language_origin='es', language_destination='en', locale_destination = 'en_US')
+translate_locale_posts(language_origin='en', language_destination='pt', locale_destination = 'pt_PT')
