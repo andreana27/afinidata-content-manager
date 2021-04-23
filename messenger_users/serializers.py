040c2ad7b43cfd9c3a6f87d6510ca984764082fb
Ubuntu <ubuntu@ip-10-0-0-202.us-east-2.compute.internal>
2021-04-23 17:34:56 +0000

    Fix serializers ReadOnlyField

--- messenger_users/serializers.py
+++ messenger_users/serializers.py
@@ -6,15 +6,15 @@ import os
 
 
 class UserSerializer(serializers.ModelSerializer):
-    profile_pic = serializers.Field()
-    last_message = serializers.Field()
-    last_bot_id = serializers.Field()
-    bot_channel_id = serializers.Field()
-    user_channel_id = serializers.Field()
-    last_seen = serializers.Field()
-    last_user_message = serializers.Field()
-    last_channel_interaction = serializers.Field()
-    window = serializers.Field()
+    profile_pic = serializers.ReadOnlyField()
+    last_message = serializers.ReadOnlyField()
+    last_bot_id = serializers.ReadOnlyField()
+    bot_channel_id = serializers.ReadOnlyField()
+    user_channel_id = serializers.ReadOnlyField()
+    last_seen = serializers.ReadOnlyField()
+    last_user_message = serializers.ReadOnlyField()
+    last_channel_interaction = serializers.ReadOnlyField()
+    window = serializers.ReadOnlyField()
 
     class Meta:
         model = User
@@ -23,21 +23,21 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserConversationSerializer(serializers.ModelSerializer):
-    profile_pic = serializers.Field()
-    last_message = serializers.Field()
-    last_bot_id = serializers.Field()
-    bot_channel_id = serializers.Field()
-    user_channel_id = serializers.Field()
-    last_seen = serializers.Field()
-    last_user_message = serializers.Field()
-    last_channel_interaction = serializers.Field()
-    window = serializers.Field()
+    profile_pic = serializers.ReadOnlyField()
+    last_message = serializers.ReadOnlyField()
+    last_bot_id = serializers.ReadOnlyField()
+    bot_channel_id = serializers.ReadOnlyField()
+    user_channel_id = serializers.ReadOnlyField()
+    last_seen = serializers.ReadOnlyField()
+    last_user_message = serializers.ReadOnlyField()
+    last_channel_interaction = serializers.ReadOnlyField()
+    window = serializers.ReadOnlyField()
 
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'username', 'last_seen', 'last_user_message',
                   'last_channel_interaction', 'window', 'user_channel_id', 'bot_channel_id', 'bot_id', 'profile_pic',
-                  'last_message']
+                  'last_message', 'last_bot_id']
 
 
 class UserDataSerializer(serializers.ModelSerializer):
