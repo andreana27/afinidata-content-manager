83074f2703edfb22b9f1322dac771adc948dd293
Gabriela Herrera <sgherrerapoggio@gmail.com>
2021-04-28 16:31:22 -0600

    Move the connectors and filter to utilities to use it in both instances and users

--- instances/api_views.py
+++ instances/api_views.py
@@ -1,18 +1,18 @@
+from datetime import datetime, timedelta, time
 from django.db.models import Q
-from rest_framework import filters
-from instances import models, serializers
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.decorators import action
 # from rest_framework.response import Response
 # from django.utils.decorators import method_decorator
 from rest_framework.pagination import PageNumberPagination
 # from rest_framework import filters
-from messenger_users.models import User
-from groups.models import ProgramAssignation, AssignationMessengerUser
-from attributes.models import Attribute
-from datetime import datetime, timedelta, time
 
+from attributes.models import Attribute
+from groups.models import ProgramAssignation, AssignationMessengerUser
+from instances import models, serializers
 from instances.models import AttributeValue
+from messenger_users.models import User, UserData
+from utilities.views import PeopleFilterSearch
 
 
 class InstanceViewSet(viewsets.ReadOnlyModelViewSet):
@@ -47,61 +47,28 @@ class InstanceViewSet(viewsets.ReadOnlyModelViewSet):
         serializer = serializers.AttributeValueFilterPosibleVal(qs, many=True)
         return pagination.get_paginated_response(serializer.data)
 
-    def apply_filter_to_search(self, field, value, condition, numeric=False):
-        # apply condition to search
-        if condition == 'is':
-            if numeric:
-                query_search = Q(**{f"{field}": value})
-            else:
-                query_search = Q(**{f"{field}__icontains": value})
-        elif condition == 'is_not':
-            if numeric:
-                query_search = ~Q(**{f"{field}": value})
-            else:
-                query_search = ~Q(**{f"{field}__icontains": value})
-        elif condition == 'startswith':
-            if numeric:
-                query_search = Q(**{f"{field}": value})
-            else:
-                query_search = Q(**{f"{field}__startswith": value})
-        elif condition == 'gt':
-            query_search = Q(**{f"{field}__gt": value})
-        elif condition == 'lt':
-            query_search = Q(**{f"{field}__lt": value})
-        return query_search
-
-    def apply_connector_to_search(self, connector, filters, query):
-        if connector is None:
-            filters = query
-        else:
-            if connector == 'and':
-                filters &= query
-            else:
-                filters |= query
-
-        return filters
-
     @action(methods=['POST'], detail=False)
     def advance_search(self, request):
         queryset = super().get_queryset()
         filtros = request.data['filtros']
         apply_filters = Q()
         next_connector = None
+        people_search = PeopleFilterSearch()
 
         for idx, f in enumerate(filtros):
             data_key = f['data_key']
             value = f['data_value']
             condition = f['condition']
             search_by = f['search_by']
-            check_attribute_type = 'INSTANCE'
+            check_attribute_type = 'USER'
 
             if search_by == 'attribute':
                 attribute = Attribute.objects.get(pk=data_key)
                 is_numeric = attribute.type == 'numeric'
 
-                # check if attribute belongs to user o instance
-                if attribute.entity_set.filter(id__in=[4, 5]).exists():
-                    check_attribute_type = 'USER'
+                # check if attribute belongs to user or instance, Priority to INSTANCES
+                if attribute.entity_set.filter(id__in=[1, 2]).exists():
+                    check_attribute_type = 'INSTANCE'
 
                 if condition == 'is_set' or condition == 'not_set':
                     if condition == 'is_set':
@@ -109,47 +76,52 @@ class InstanceViewSet(viewsets.ReadOnlyModelViewSet):
                         if check_attribute_type == 'USER':
                             users = User.objects.filter(Q(userdata__attribute_id=data_key))
                             q = Q(instanceassociationuser__user_id__in=users)
-                            apply_filters = self.apply_connector_to_search(next_connector, apply_filters, q)
+                            apply_filters = people_search.apply_connector(next_connector, apply_filters, q)
                         else:
                             q = Q(attributevalue__attribute_id=data_key)
-                            apply_filters = self.apply_connector_to_search(next_connector, apply_filters, q)
+                            apply_filters = people_search.apply_connector(next_connector, apply_filters, q)
 
                     if condition == 'not_set':
                         # validar not set attribute
                         if check_attribute_type == 'USER':
                             users = User.objects.filter(~Q(userdata__attribute_id=data_key))
                             q = Q(instanceassociationuser__user_id__in=users)
-                            apply_filters = self.apply_connector_to_search(next_connector, apply_filters, q)
+                            apply_filters = people_search.apply_connector(next_connector, apply_filters, q)
                         else:
                             q = ~Q(attributevalue__attribute_id=data_key)
-                            apply_filters = self.apply_connector_to_search(next_connector, apply_filters, q)
+                            apply_filters = people_search.apply_connector(next_connector, apply_filters, q)
                 else:
 
                     if check_attribute_type == 'USER':
                         # filter by attribute user
-                        s = self.apply_filter_to_search('userdata__data_value', value, condition, numeric=is_numeric)
-                        qs = User.objects.filter(s)  # .values_list('id',flat=True)
-                        query = Q(instanceassociationuser__user_id__in=qs)
-                        apply_filters = self.apply_connector_to_search(next_connector, apply_filters, query)
+                        last_attributes = people_search.get_last_attributes(data_key, model=UserData, type_id='user_id')
+
+                        s = people_search.apply_filter('userdata__data_value', value, condition, numeric=is_numeric)
+                        s = s & Q(userdata__id__in=last_attributes)
+                        query_search = list(User.objects.filter(s).values_list('id',flat=True))
+                        query = Q(instanceassociationuser__user_id__in=query_search)
                     else:
                         # filter by attribute instance
-                        query_search = self.apply_filter_to_search('attributevalue__value', value, condition, numeric=is_numeric)
-                        query = Q(attributes__id=data_key) & query_search
-                        apply_filters = self.apply_connector_to_search(next_connector, apply_filters, query)
+                        last_attributes = people_search.get_last_attributes(data_key, model=InstanceAttributeValue, type_id='instance_id')
+
+                        query_search = people_search.apply_filter('attributevalue__value', value, condition, numeric=is_numeric)
+                        query = query_search & Q(attributevalue__id__in=last_attributes)
+                        
+                    apply_filters = people_search.apply_connector(next_connector, apply_filters, query)
 
             elif search_by == 'bot':
                 condition = condition if condition == 'is' else 'is_not'
-                s = self.apply_filter_to_search('user__bot_id', value, condition, numeric=True)
+                s = people_search.apply_filter('user__bot_id', value, condition, numeric=True)
                 qs = AssignationMessengerUser.objects.filter(s).values_list('user_id', flat=True).exclude(user_id__isnull=True).distinct()
                 query = Q(instanceassociationuser__user_id__in=list(qs))
-                apply_filters = self.apply_connector_to_search(next_connector, apply_filters, query)
+                apply_filters = people_search.apply_connector(next_connector, apply_filters, query)
             
             elif search_by == 'channel':
                 # filter by channel
-                s = self.apply_filter_to_search('channel_id', value, condition)
+                s = people_search.apply_filter('channel_id', value, condition)
                 qs = User.objects.filter(s).order_by('-id').values_list('id', flat=True)
                 query = Q(instanceassociationuser__user_id__in=list(qs))
-                apply_filters = self.apply_connector_to_search(next_connector, apply_filters, query)
+                apply_filters = people_search.apply_connector(next_connector, apply_filters, query)
             
             elif search_by == 'dates':
                 # filter by dates
@@ -161,17 +133,17 @@ class InstanceViewSet(viewsets.ReadOnlyModelViewSet):
 
             elif search_by == 'group':
                 # filter by group
-                s = self.apply_filter_to_search('group__id', value, condition)
+                s = people_search.apply_filter('group__id', value, condition)
                 qs = AssignationMessengerUser.objects.filter(s).values_list('user_id', flat=True).exclude(user_id__isnull=True).distinct()
                 query_group = Q(instanceassociationuser__user_id__in=list(qs))
-                apply_filters = self.apply_connector_to_search(next_connector, apply_filters, query_group)
+                apply_filters = people_search.apply_connector(next_connector, apply_filters, query_group)
 
             elif search_by == 'program':
                 # filter by program
-                s = self.apply_filter_to_search('program__id', value, condition)
+                s = people_search.apply_filter('program__id', value, condition)
                 qs = ProgramAssignation.objects.filter(s).values_list('user_id', flat=True).exclude(user_id__isnull=True)
                 query = Q(instanceassociationuser__user_id__in=list(qs))
-                apply_filters = self.apply_connector_to_search(next_connector, apply_filters, query)
+                apply_filters = people_search.apply_connector(next_connector, apply_filters, query)
 
             next_connector = f['connector']
 
